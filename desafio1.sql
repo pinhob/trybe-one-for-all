a73d25b67727922ba8710f295f859c5ab51b74aa
Bruno Pinho <brunopinho@outlook.com>
2021-10-30 19:37:03 -0300

    feito requisito 3. criado arquivos dos requisitos restantes

--- desafio1.sql
+++ desafio1.sql
@@ -94,7 +94,7 @@ VALUES
     ('Troubles Of My Inner Fire', 2),
     ('Time Fireworks', 2),
     ('Magic Circus', 3),
-    ('Honey So Do I', 3),
+    ('Honey, So Do I', 3),
     ("Sweetie Let's Go Wild", 3),
     ('She Knows', 3),
     ('Fantasy For Me', 4),
@@ -102,7 +102,7 @@ VALUES
     ('Rock His Everything', 4),
     ('Home Forever', 4),
     ('Diamond Power', 4),
-    ("Honey Let's Be Silly", 4),
+    ("Honey, Let's Be Silly", 4),
     ('Thang Of Thunder', 5),
     ('Words Of Her Life', 5),
     ('Without My Streets', 5);
